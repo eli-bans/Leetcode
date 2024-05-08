09c3b5fe9b8ba0ad2482b8adc0db94794eecd18a
EB <114438934+eli-bans@users.noreply.github.com>
2024-05-08 10:33:36 +0000

    Attempted the PRAMP question AbsSort()

---
+++ Pramp_absSort.py
@@ -0,0 +1,11 @@
+def absSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - 1):
+            if abs(arr[j]) > abs(arr[j + 1]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+            elif abs(arr[j]) == abs([j + 1]) and arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
